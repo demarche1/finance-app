028e0d45620bcb293607e4a3339cd073117c5145
demarche1 <alessandro.demarche00@gmail.com>
2022-03-17 21:47:19 -0300

    feat: added ts vue js project

--- bitcoin-candle-frontend/src/components/HelloWorld.vue
+++ bitcoin-candle-frontend/src/components/HelloWorld.vue
@@ -20,6 +20,14 @@
       </li>
       <li>
         <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
+          target="_blank"
+          rel="noopener"
+          >typescript</a
+        >
+      </li>
+      <li>
+        <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
           target="_blank"
           rel="noopener"
@@ -94,13 +102,17 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "HelloWorld",
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
   props: {
     msg: String,
   },
-};
+})
+export default class HelloWorld extends Vue {
+  msg!: string;
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
