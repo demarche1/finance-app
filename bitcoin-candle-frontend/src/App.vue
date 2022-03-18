c0d52502e53a096e02cac739b1750ce4ea1080b7
demarche1 <alessandro.demarche00@gmail.com>
2022-03-17 22:27:15 -0300

    feat: add frontend dependencies

--- bitcoin-candle-frontend/src/App.vue
+++ bitcoin-candle-frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
 <script lang="ts">
--- bitcoin-candle-frontend/src/main.ts
+++ bitcoin-candle-frontend/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
+import VueApexCharts from "vue3-apexcharts";
 import App from "./App.vue";
 import store from "./store";
 
-createApp(App).use(store).mount("#app");
+const app = createApp(App);
+
+app.use(store);
+app.use(VueApexCharts);
+app.mount("#app");
